7ddbc3a2354482678444adf212d1a2e42255e518
Abdallah Abis <Abis.Abdallah@gmail.com>
2020-04-25 23:14:05 +0000

    600px Media Query implemented to ContactPage, because I forgote it lol

--- src/pages/contactUsPage/contactUsPage.styles.jsx
+++ src/pages/contactUsPage/contactUsPage.styles.jsx
@@ -1,21 +1,31 @@
 import styled from "styled-components";
 
+import device from "../../assets/styles/media";
 import variables from "../../assets/styles/variables.scss";
 
 import Input from "../../components/input/input.component";
 import CustomButton from "../../components/custom-button/custom-button.component";
 
 export const Container = styled.div`
-  margin: 8rem 10rem 0 10rem;
+  padding: 8rem 10rem 0 10rem;
   padding-top: 6vw;
   padding-bottom: 10rem;
   display: flex;
   justify-content: space-between;
+
+  @media ${device.phone} {
+    padding: 4rem 5rem 0 5rem;
+    flex-direction: column;
+    height: 100vh;
+  }
 `;
 
 export const InputField = styled(Input)`
   margin-bottom: 3rem;
   width: 70%;
+  @media ${device.phone} {
+    width: 100%;
+  }
 `;
 
 export const Button = styled(CustomButton)`
@@ -25,6 +35,9 @@ export const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 50%;
+  @media ${device.phone} {
+    width: 100%;
+  }
 `;
 export const Title = styled.h1`
   font-family: ${variables.viga};
@@ -35,6 +48,10 @@ export const DeveloperContainer = styled.div`
   padding: 3rem 5rem;
   border: 3px solid ${variables.lightGrey};
   border-radius: 5px;
+
+  @media ${device.phone} {
+    margin-top: 10rem;
+  }
 `;
 export const SubTitle = styled.h2`
   font-family: ${variables.montserrat};
